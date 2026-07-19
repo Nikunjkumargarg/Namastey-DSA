504b63113641096c48fd37a938aa4c2626d73eab
NikunjkGarg <nikunjkumar@eupotic.io>
2026-07-19 14:09:59 +0530

    Day-3 understand the remove duplicates from non decreasing array

---
+++ Day-2/Arrays - Easy-Medium/removeDupliatesFromSortedArray.js
@@ -0,0 +1,40 @@
+//solution 1.
+// let arr = [1,2,3,3,4,5,6,6,6,6,6,8,9,10,10]
+// let i = 0
+// let pointer = 0
+// while(i < arr.length -1)
+// {
+//     if(arr[i] != arr[i+1])
+//     {
+//         pointer ++
+//         arr[pointer] = arr[i+1]
+//     }
+//     i++
+// }
+
+// console.log(pointer+1)
+
+//Note: In above algorithm, if array contains only 1 value then this code will give error "if(arr[i] != arr[i+1])"
+
+let arr = [1]
+
+function removeDuplicatesFromSortedArray(arr)
+{
+    let pointer = 0
+    if(arr.length<1)
+    {
+        return 0
+    }
+    for(let i=0; i< arr.length; i++)
+    {
+        if(arr[i]>arr[pointer])
+        {
+            pointer++
+            arr[pointer] = arr[i]
+        }
+    }
+    return pointer+1
+
+}
+
+console.log(removeDuplicatesFromSortedArray(arr))
