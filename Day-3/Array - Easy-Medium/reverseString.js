3f57f8c3ea45cd511f9e38bdf4bfdf2190c872b2
NikunjkGarg <nikunjkumar@eupotic.io>
2026-07-19 15:14:48 +0530

    Day-3 reverse string algorith fix

--- Day-3/Array - Easy-Medium/reverseString.js
+++ Day-3/Array - Easy-Medium/reverseString.js
@@ -2,16 +2,13 @@ let arr = ["h","e","l","l","o"]
 
 function reverseString(arr)
 {
-    let pointer1 = 0
-    let pointer2 = arr.length-1
-
-    while(pointer1 != pointer2)
+    let halfLength = Math.floor(arr.length/2)
+    
+    for(let i=0; i<halfLength; i++)
     {
-        let tempValue = arr[pointer1]
-        arr[pointer1] = arr[pointer2]
-        arr[pointer2] = tempValue
-        pointer1++
-        pointer2--
+        let tempValue = arr[i]
+        arr[i] = arr[arr.length-1-i]
+        arr[arr.length-1-i] = tempValue
     }
     return arr
 }
