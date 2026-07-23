f95316812fc2bc91f9b03ed2c77874887a7407b6
NikunjkGarg <nikunjkumar@eupotic.io>
2026-07-23 11:15:58 +0530

    Day-7 binary search time and space complexity node add

--- Day-6 (22-07-26)/binarySearch.js
+++ Day-6 (22-07-26)/binarySearch.js
@@ -2,8 +2,8 @@
 // 1. array should be sorted
 // find the mid and compare with target and adjust the left and right values per target.
 // base case is if right greater than left that means target not present or not found.
-let arr = [2,3,4,5,6,7]
-let target = 7
+let arr = [2]
+let target = 2
 
 
 function search(arr,target)
@@ -33,3 +33,6 @@ function search(arr,target)
 }
 
 console.log(search(arr,target))
+
+// time complexity = o(log n)
+// space complexity = o(1)
