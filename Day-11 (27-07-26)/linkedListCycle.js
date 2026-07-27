1c469f894e44e8a59ce96c77f537eff697484f65
NikunjkGarg <nikunjkumar@eupotic.io>
2026-07-27 11:08:03 +0530

    Day-11 linked list cycle (using floyd's cycle detecting algorithm using slow and fast pointer approach)

--- Day-11 (27-07-26)/linkedListCycle.js
+++ Day-11 (27-07-26)/linkedListCycle.js
@@ -1,20 +1,41 @@
 //  Approach one: time complexity - o(n), space complexity - o(n)
 
- function listCycle (head)
- {
-    let nodeSet = new Set()
-    let curr = head
+//  function listCycle (head)
+//  {
+//     let nodeSet = new Set()
+//     let curr = head
 
-    while(curr)
+//     while(curr)
+//     {
+//         if(nodeSet.has(curr))
+//         {
+//             return true
+//         }
+//         nodeSet.add(curr)
+//         curr = curr.next
+//     }
+//     return false
+//  }
+
+// Approach two: Using Floyd's cycle finding algorithm.
+// Two pointers : (if cycle exists, then slow point == fast pointer at some point in time)
+// slow and fast pointer approach
+// Time complexity - o(n), space complexity - o(1)
+
+function listCycle(head)
+{
+    let slow = head
+    let fast = head
+
+    while(fast && fast.next)
     {
-        if(nodeSet.has(curr))
+        slow = slow.next // move one step
+        fast = fast.next.next // move two step
+        if(slow === fast)
         {
             return true
         }
-        nodeSet.add(curr)
-        curr = curr.next
     }
     return false
- }
+}
 
- 
