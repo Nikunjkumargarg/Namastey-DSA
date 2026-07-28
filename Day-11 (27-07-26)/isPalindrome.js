acacf47ee24b444b8640f77673d249c042f4fd2e
NikunjkGarg <nikunjkumar@eupotic.io>
2026-07-28 11:47:39 +0530

    Day-12 isPalindrome linked list

---
+++ Day-11 (27-07-26)/isPalindrome.js
@@ -0,0 +1,50 @@
+function isPalindrome(head)
+{
+    if(!head || !head.next)
+    {
+        return true
+    }
+
+    //find middle of linked list
+
+    let slow = head
+    let fast = head
+
+    while(fast && fast.next)
+    {
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    if(fast)
+    {
+        slow = slow.next
+    }
+
+    // reverse second half linked list
+
+    let prev = null
+    let curr = slow
+
+    while(curr)
+    {
+        let next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+
+    // compare linked lists
+    let first = head
+    let second = prev
+    while(second)
+    {
+        if(first.val !== second.val)
+        {
+            return false
+        }
+        first = first.next
+        second = second.next
+    }
+    return true
+}
