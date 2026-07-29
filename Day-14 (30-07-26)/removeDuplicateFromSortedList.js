36983e6ff27e7e15de965492a6fe89100bc3f654
NikunjkGarg <nikunjkumar@eupotic.io>
2026-07-30 02:11:49 +0530

    Day-14 remove duplicates from sorted list

---
+++ Day-14 (30-07-26)/removeDuplicateFromSortedList.js
@@ -0,0 +1,19 @@
+function deleteDuplicate(head)
+{
+    let sentinalNode = new Node()
+    sentinalNode.next = head
+
+    while(head && head.next)
+    {
+        if(head.val === head.next.val)
+        {
+            head.next = head.next.next
+        }
+        else
+        {
+            head = head.next
+        }
+    }
+
+    return sentinalNode.next
+}
