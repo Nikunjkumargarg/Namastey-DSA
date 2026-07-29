1aa5058a18c297ed1c38be3c79d46951d5a7810c
NikunjkGarg <nikunjkumar@eupotic.io>
2026-07-30 01:58:25 +0530

    Day-14 remove nth element from last of linked list (in one step)

--- Day-13 (29-07-26)/removeNthFromEnd.js
+++ Day-13 (29-07-26)/removeNthFromEnd.js
@@ -1,22 +1,53 @@
+//Approach 1 - traversing linked list twice (1. find length, 2. reaching the position to delete the node)
+// time complexity - o(n)
+// space complexity - o(1)
+
+
+// function removeNthFromEnd(head,n)
+// {
+//     let sentinalNode = new Node()
+//     sentinalNode.next = head
+
+//     let length = 0
+
+//     while(head)
+//     {
+//         length++
+//         head = head.next
+//     }
+
+//     let prevpos = length-n;
+//     let prev = sentinalNode
+//     for(let i=0; i<prevpos; i++)
+//     {
+//         prev = prev.next
+//     }
+//     prev = prev.next.next
+//     return sentinalNode.next
+// }
+
+// Approach 2: one pass solution, only one loop (two pointer move the fast pointer by n steps)
+
 function removeNthFromEnd(head,n)
 {
     let sentinalNode = new Node()
     sentinalNode.next = head
-    let length = 0
-    while(head)
+
+    let slow = sentinalNode
+    let fast = sentinalNode;
+    let move = 0
+    while(move < n)
     {
-        head = head.next
-        length++
+        fast = fast.next
+        move++
     }
 
-    let prevpos = (length-n)
-
-    let prev = sentinalNode
-
-    for(let i=0; i< prevpos; i++)
+    while(fast && fast.next)
     {
-        prev = prev.next
+        slow = slow.next
+        fast = fast.next
     }
-    prev.next = prev.next.next;
-    return sentinalNode.next;
+
+    slow.next = slow.next.next
+    return sentinalNode.next
 }
