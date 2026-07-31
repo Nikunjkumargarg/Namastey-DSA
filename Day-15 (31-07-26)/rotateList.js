219303862293309cc9b7e21dc9588d0ad01c0ece
NikunjkGarg <nikunjkumar@eupotic.io>
2026-07-31 23:05:45 +0530

    Day -15 rotate linked list k times

---
+++ Day-15 (31-07-26)/rotateList.js
@@ -0,0 +1,39 @@
+function rotateList(head,k)
+{
+    if(!head || !head.next) return head
+
+    let len = 0
+    let curr = head
+    while(curr)
+    {
+        curr = curr.next
+        len++
+    }
+
+    k = k % len
+
+    if(k===0)
+    {
+        return head
+    }
+
+    let slow = head
+    let fast = head
+
+    for(let i=0; i<k; i++)
+    {
+        fast = fast.next
+    }
+
+    while(fast.next)
+    {
+        slow = slow.next
+        fast = fast.next
+    }
+
+    let currHead = slow.next
+    slow.next = null
+    fast.next = head
+
+    return currHead
+}
