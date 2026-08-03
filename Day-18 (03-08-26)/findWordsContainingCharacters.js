3552e5a8cd39396f084f53bbc0d4393efde6542c
NikunjkGarg <nikunjkumar@eupotic.io>
2026-08-03 15:07:36 +0530

    Day-18 find words containing characters

---
+++ Day-18 (03-08-26)/findWordsContainingCharacters.js
@@ -0,0 +1,17 @@
+function findWordsContiningCharacters(words,x)
+{
+    let result = []
+
+    for(let i=0; i<words.length; i++)
+    {
+        for(let j=0; j<words[i].length; j++)
+        {
+            if(words[i][j] === x)
+            {
+                result.push(i)
+                break
+            }
+        }
+    }
+    return result
+}
