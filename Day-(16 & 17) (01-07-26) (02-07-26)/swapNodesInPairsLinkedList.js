66f1aa6fee81731309976a5d4caaff43dcb15171
NikunjkGarg <nikunjkumar@eupotic.io>
2026-08-03 11:42:26 +0530

    Day-18 swap node in pairs linked list (iterative approach)

--- Day-(16 & 17) (01-07-26) (02-07-26)/swapNodesInPairsLinkedList.js
+++ Day-(16 & 17) (01-07-26) (02-07-26)/swapNodesInPairsLinkedList.js
@@ -11,19 +11,20 @@ function swapNodesInpairs(head)
 
     dummy.next = head
 
-    let p = dummy
+    let prev = dummy
     let c = head
     let n = head.next
 
-    while(c && n)
+    while(n && c)
     {
-        p.next = n
+        prev.next = n
         c.next = n.next
         n.next = c
 
-        p = c
-        c = p.next
+        prev = c
+        c = c.next
         n = c && c.next
+
     }
 
     return dummy.next
