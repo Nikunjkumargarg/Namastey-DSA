6056400e317f0c8001d46a43c1781345d485b4f6
NikunjkGarg <nikunjkumar@eupotic.io>
2026-08-03 14:21:13 +0530

    Day 18 approach two using one loop to solve length of last word in a string

--- Day-18 (03-08-26)/lengthOfLastWordInString.js
+++ Day-18 (03-08-26)/lengthOfLastWordInString.js
@@ -1,3 +1,5 @@
+// Approach 1: using two loops
+
 var lengthOfLastWord = function(s)
 {
     if(!s) return 0
@@ -16,5 +18,27 @@ var lengthOfLastWord = function(s)
         count++
     }
 
+    return count
+}
+
+// Approach 2: using one loop
+
+var lengthOfLastWord = function(s)
+{
+    let i = s.length-1
+    let count = 0
+
+    while(i>=0)
+    {
+        if(s[i] != " ")
+        {
+            count++
+        }
+        else if(count > 0)
+        {
+            break;
+        }
+        i--
+    }
     return count
 }
