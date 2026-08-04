c87f72093aad7e9c8a33826b85a3efbe3fe52784
NikunjkGarg <nikunjkumar@eupotic.io>
2026-08-05 00:58:37 +0530

    Day-20 jewels and stones problems(3 approaches)

---
+++ Day-19 (04-08-26)/jewelsAndStones.js
@@ -0,0 +1,55 @@
+// Approach 1: Brute force
+
+// function jewelAndStones(jewel, stone)
+// {
+//     let count = 0
+//     for(let i=0; i<stone.length; i++)
+//     {
+//         if(jewel.includes(stone[i]))
+//         {
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+
+//Approach 1: not using includes function but loop
+// function jewelAndStones(jewel, stone)
+// {
+//     let count = 0
+//     for(let i=0; i<stone.length; i++)
+//     {
+//         for(let j=0; j<jewel.length; j++)
+//         {
+//             if(jewel[j] === stone[i])
+//             {
+//                 count++
+//                 break 
+//             }
+//         }
+//     }
+//     return count
+// }
+
+// Approach 2 : using set
+// Set removes duplicates
+function jewelAndStones(jewel, stone)
+{
+    let count = 0
+    let set = new Set()
+
+    for(let i=0; i<jewel.length; i++)
+    {
+        set.add(jewel[i])
+    }
+
+    for(let i=0; i<stone.length; i++)
+    {
+        if(set.has(stone[i]))
+        {
+            count++
+        }
+    }
+    return count
+}
