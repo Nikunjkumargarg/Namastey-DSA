c263dbcfe8a76dc80d7d3cff4058e1914bb2c6f5
NikunjkGarg <nikunjkumar@eupotic.io>
2026-08-07 22:17:31 +0530

    Day-22 valid palindrom approach 2 (reversing string using loop rather than javascript built in funcition "reverse")

--- Day-22 (07-08-26)/validPalindrome.js
+++ Day-22 (07-08-26)/validPalindrome.js
@@ -1,3 +1,5 @@
+//Approach 1: using reverse javascript method
+
 function validPalindrome(s)
 {
     s = s.toLowerCase()
@@ -5,6 +7,8 @@ function validPalindrome(s)
     let filteredString =""
     for(let i=0; i<s.length; i++)
     {
+        //condition to check the a-z and 0-9
+        // if((s[i].charCodeAt() >= 'a' && s[i].charCodeAt() <= 'z') || (s[i].charCodeAt() >= 0 && s[i].charCodeAt() <= '9'))
         if(s[i].match(/[a-z0-9]/i))
         {
             filteredString += s[i]
@@ -14,3 +18,27 @@ function validPalindrome(s)
     rev = filteredString.split("").reverse().join("")
     return filteredString === rev
 }
+
+//Approach 2: using loop itself for both filter and reversing the string
+function validPalindrome(s)
+{
+    s = s.toLowerCase()
+    let rev = ""
+    let filteredString =""
+
+    for(let i=0; i<s.length; i++)
+    {
+        //condition to check the a-z and 0-9
+        // if((s[i].charCodeAt() >= 'a' && s[i].charCodeAt() <= 'z') || (s[i].charCodeAt() >= 0 && s[i].charCodeAt() <= '9'))
+        if(s[i].match(/[a-z0-9]/i))
+        {
+            filteredString = filteredString + s[i]
+            rev = s[i] + rev
+        }
+    }
+    return filteredString === rev
+}
+
+//time complexity = o(n)
+//space complexity - o(n)
+
