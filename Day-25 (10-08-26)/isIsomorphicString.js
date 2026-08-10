e8e982be4593ce467c6e340843f685ee2f56956e
NikunjkGarg <nikunjkumar@eupotic.io>
2026-08-10 23:01:16 +0530

    Day-25 extra condition removed reduncdant condition. is string isomorphic

--- Day-25 (10-08-26)/isIsomorphicString.js
+++ Day-25 (10-08-26)/isIsomorphicString.js
@@ -10,12 +10,9 @@ var isIsomorphic = function(s, t) {
         {
             mapStoT[s[i]] = t[i]
             mapTtoS[t[i]] = s[i]
+
         }
-        else if(mapTtoS[t[i]] !== s[i])
-        {
-            return false
-        }
-        else if(mapStoT[s[i]] !== t[i] )
+        else if(mapStoT[s[i]] !== t[i])
         {
             return false
         }
