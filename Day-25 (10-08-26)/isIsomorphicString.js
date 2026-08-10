645bf3f0614f7af6c3de341a510699bfde0862ee
NikunjkGarg <nikunjkumar@eupotic.io>
2026-08-10 22:47:30 +0530

    Day-25 is isomorphic string

---
+++ Day-25 (10-08-26)/isIsomorphicString.js
@@ -0,0 +1,24 @@
+var isIsomorphic = function(s, t) {
+    let mapStoT = {}
+    let mapTtoS = {}
+
+    if(s.length !== t.length) return false
+
+    for(let i=0; i<s.length; i++)
+    {
+        if(!mapStoT[s[i]] && !mapTtoS[t[i]])
+        {
+            mapStoT[s[i]] = t[i]
+            mapTtoS[t[i]] = s[i]
+        }
+        else if(mapTtoS[t[i]] !== s[i])
+        {
+            return false
+        }
+        else if(mapStoT[s[i]] !== t[i] )
+        {
+            return false
+        }
+    }
+    return true
+};
