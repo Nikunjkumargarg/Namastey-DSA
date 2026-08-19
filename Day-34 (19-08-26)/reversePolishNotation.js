4480f5f40cd8101ad2bc0e5c64f9554e99bb5b36
NikunjkGarg <nikunjkumar@eupotic.io>
2026-08-19 21:48:57 +0530

    Day-34 reverse polish notation (using stack)

--- Day-34 (19-08-26)/reversePolishNotation.js
+++ Day-34 (19-08-26)/reversePolishNotation.js
@@ -1,4 +1,38 @@
-function reversePolishNotation()
+function reversePolishNotation(tokens)
 {
-    
+    let operators = ["*","+","-","/"]
+    let stack = []
+
+    for(let i=0; i<tokens.length; i++)
+    {
+        if(!operators.includes(tokens[i]))
+        {
+            stack.push(Number(tokens[i]))
+        }
+        else
+        {
+            let second = stack.pop()
+            let first = stack.pop()
+            let result = 0
+
+            switch(tokens[i])
+            {
+                case "+":
+                    result = first + second
+                    break
+                case "-":
+                    result = first - second
+                    break
+                case "/":
+                    result = Math.trunc(first / second)
+                    break
+                case "*":
+                    result = first * second
+                    break
+            }
+
+            stack.push(result)
+        }
+    }
+    return stack.pop()
 }
